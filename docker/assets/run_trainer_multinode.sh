1072117c9c3e66c2a0e878516f32c309f783d381
Zhenguo Chen <czhenguo@amazon.com>
2025-03-06 00:05:53 +0000

    run custom experiments

--- docker/assets/run_trainer_multinode.sh
+++ docker/assets/run_trainer_multinode.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TEST_ARTIFACTS_PATH="/shared/axlearn_artifacts/$POD_UID/"
+TEST_ARTIFACTS_PATH="/shared/czhenguo/Projects/fruitstand/run_artifacts/$POD_UID/"
 mkdir -p "$TEST_ARTIFACTS_PATH"
 NEURON_DUMP_PATH=${TEST_ARTIFACTS_PATH}/neuron_dump/$OMPI_COMM_WORLD_RANK
 HLO_DUMP_PATH=${TEST_ARTIFACTS_PATH}/hlo_dump
@@ -37,14 +37,36 @@ else
     exit 1
 fi
 
+PYINSTRUMENT_OUTPUT_PATH="${TEST_ARTIFACT_PATH}/recovery.pyisession"
+
+# Sync changes
+git config --global --add safe.directory /shared/czhenguo/Projects/fruitstand/axlearn
+cd /shared/czhenguo/Projects/fruitstand/axlearn
+git diff czhenguo/scale-out --no-color > changes.patch
+echo "patch generated"
+cd /neuron/axlearn
+echo "applying patch"
+git apply /shared/czhenguo/Projects/fruitstand/axlearn/changes.patch
+cd /neuron
+
 # show env vars in logs
 set
 
 # Run the training script
-python3 -m axlearn.common.launch_trainer_main \
-    --module=text.gpt.c4_trainer --config=fuji-70B-v2-flash \
-    --trainer_dir=$OUTPUT_DIR --data_dir=$DATA_DIR \
-    --jax_backend=neuron --mesh_selector=neuron-trn2.48xlarge-64 \
-    --distributed_coordinator=$COORDINATOR_ADDRESS \
-    --num_processes=$OMPI_COMM_WORLD_SIZE \
-    --process_id=$OMPI_COMM_WORLD_RANK 2>&1 | tee ${OUTPUT_DIR}/${PMIX_HOSTNAME}.log
+if [ $NEURON_PJRT_PROCESS_INDEX == 0 ]; then
+    pyinstrument -o $PYINSTRUMENT_OUTPUT_PATH --hide-regex ".*traceback_util\.py" -m axlearn.common.launch_trainer_main \
+        --module=text.gpt.c4_trainer --config=fuji-70B-v2-flash \
+        --trainer_dir=$OUTPUT_DIR --data_dir=$DATA_DIR \
+        --jax_backend=neuron --mesh_selector=neuron-trn2.48xlarge-64 \
+        --distributed_coordinator=$COORDINATOR_ADDRESS \
+        --num_processes=$OMPI_COMM_WORLD_SIZE \
+        --process_id=$OMPI_COMM_WORLD_RANK 2>&1 | tee ${OUTPUT_DIR}/${PMIX_HOSTNAME}.log
+else
+    pyinstrument -o $PYINSTRUMENT_OUTPUT_PATH --hide-regex ".*traceback_util\.py" -m axlearn.common.launch_trainer_main \
+        --module=text.gpt.c4_trainer --config=fuji-70B-v2-flash \
+        --trainer_dir=$OUTPUT_DIR --data_dir=$DATA_DIR \
+        --jax_backend=neuron --mesh_selector=neuron-trn2.48xlarge-64 \
+        --distributed_coordinator=$COORDINATOR_ADDRESS \
+        --num_processes=$OMPI_COMM_WORLD_SIZE \
+        --process_id=$OMPI_COMM_WORLD_RANK 2>&1 | tee ${OUTPUT_DIR}/${PMIX_HOSTNAME}.log
+fi
