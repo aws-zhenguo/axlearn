445d2db1c24662dc03174c29efc25e0a94a1e60e
Zhenguo Chen <czhenguo@amazon.com>
2025-03-11 18:59:43 +0000

    add custom launch config

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -6,8 +6,8 @@ else
     echo "PROJECT_PATH set as ${PROJECT_PATH}"
 fi
 
-TAG=axlearn_neuronx
-docker build --build-arg PROJECT_PATH_ARG=$PROJECT_PATH docker -t $TAG
+TAG=axlearn_neuronx-czhenguo
+docker build --build-arg PROJECT_PATH_ARG=$PROJECT_PATH --no-cache docker -t $TAG
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 REGION=us-east-2
